e34cec90ecd463a318acfab90b3a6c8b7b4abc6d
ashwinm-oo7 <ashwinmaurya9211@gmail.com>
2026-03-19 17:59:20 +0530

    admin user mcq11qbac not blockf12jas

--- src/auth/axiosInstance.js
+++ src/auth/axiosInstance.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_URL,
+  // baseURL: process.env.REACT_APP_URL,
+  baseURL: "",
+
   withCredentials: true,
 });
 
